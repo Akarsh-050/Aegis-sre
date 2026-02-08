e8f7daa890b06388f197ee4bb4b58d779b9fec92
Akarsh <akarshmishra050@gmail.com>
2026-02-08 13:51:58 +0530

    feat/loop/memory/HITL workflow added

--- src/graph/workflow.ts
+++ src/graph/workflow.ts
@@ -1,7 +1,17 @@
-import { StateGraph, START, END } from "@langchain/langgraph";
+import { StateGraph, START, END , MemorySaver } from "@langchain/langgraph";
 import { AgentState } from "./state.js";
 import { analyzerNode } from "../agents/diagnoser.js";
 import { programmerNode } from "../agents/programmer.js";
+
+// conditional validation
+function shouldContinue(state: typeof AgentState.State) {
+  // If it's fixed or we've tried 3 times, stop.
+  if (state.isFixed || state.attempts >= 3) {
+    return END;
+  }
+  // Otherwise, try to analyze the NEW state of the app
+  return "analyze";
+}
 
 // 1. Initialize the Graph with our Memory Schema
 const workflow = new StateGraph(AgentState)
@@ -14,7 +24,15 @@ const workflow = new StateGraph(AgentState)
   // Logic: Start -> Analyze -> Patch -> End
   .addEdge(START, "analyze")
   .addEdge("analyze", "patch")
-  .addEdge("patch", END);
+
+  // 3. The Conditional Edge (The Loop)
+  // After 'patch', call 'shouldContinue' to decide where to go
+  .addConditionalEdges("patch", shouldContinue);
 
 // 4. Compile the graph into a runnable "App"
-export const aegisBrain = workflow.compile();
+//    The Safety Breakpoint (Human-in-the-loop)
+//    checkpointer added 
+export const aegisBrain = workflow.compile({
+  checkpointer : new MemorySaver(),  // imMemory checkpointing for simplicity;
+  interruptBefore: ["patch"],
+});
